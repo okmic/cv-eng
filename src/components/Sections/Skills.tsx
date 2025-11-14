6f291cedc2d72d40f149ce724424d3d1ec5fc187
okmic <okmic.g@gmail.com>
2025-11-14 19:50:08 +0300

    fix mobl bugs

--- src/components/Sections/Skills.tsx
+++ src/components/Sections/Skills.tsx
@@ -35,12 +35,11 @@ export default function Skills() {
                 {skills.title}
             </span>
           </div>
-          <h2 className="text-3xl sm:text-5xl lg:text-6xl font-bold text-white mb-4">
+          <h2 className="breack-all text-2xl sm:text-4xl  lg:text-6xl font-bold text-white mb-4">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400">
               {skills.title1.split(' ')[0]}
             </span>
-            {' '}
-            <span className="text-white">{skills.title.split(' ')[1]}</span>
+            <span className="text-white">{skills.title1.split(' ')[1]}</span>
           </h2>
           <p className="text-base sm:text-lg text-gray-400 max-w-2xl mx-auto">
             {skills.subtitle1}
